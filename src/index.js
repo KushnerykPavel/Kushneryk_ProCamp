9117e672185d3e050ca3342b8f4b9b1e0c79a6c7
KushnerykPavel <levelup.kpi@gmail.com>
2019-07-10 22:04:27 +0300

    Setup Persist Reducer

--- src/index.js
+++ src/index.js
@@ -8,13 +8,24 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
+import { persistStore, persistReducer } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
+import storage from 'redux-persist/lib/storage'
+
 import rootReducer from './store/rootReducer';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
+const persistedReducer = persistReducer(persistConfig, rootReducer, composeEnhancers(applyMiddleware(thunk)))
+const store = createStore(persistedReducer, composeEnhancers(applyMiddleware(thunk)))
+let persistor = persistStore(store)
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><PersistGate loading={null} persistor={persistor}><App /></PersistGate></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

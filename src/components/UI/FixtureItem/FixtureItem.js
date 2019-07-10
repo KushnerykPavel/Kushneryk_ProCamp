0c4ae549d8d18c43317abbd5b1cc2f9118258909
KushnerykPavel <levelup.kpi@gmail.com>
2019-07-10 21:28:06 +0300

    Add fixture expansion panel

--- src/components/UI/FixtureItem/FixtureItem.js
+++ src/components/UI/FixtureItem/FixtureItem.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import ButtonBase from '@material-ui/core/ButtonBase';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -15,7 +18,7 @@ const useStyles = makeStyles(theme => ({
     paper: {
         padding: theme.spacing(2),
         margin: 'auto',
-        maxWidth: 500,
+        maxWidth: '100%',
     },
     image: {
         width: 128,
@@ -29,52 +32,75 @@ const useStyles = makeStyles(theme => ({
     },
 }));
 
-const FixtureItem = props => {
+const FixtureExpansionPanel = props => {
     const classes = useStyles();
 
     return (
         <div className={classes.root}>
-            <Paper className={classes.paper}>
-                <Grid container spacing={4}>
-                    <Grid item>
-                        <ButtonBase className={classes.image}>
-                            <Link to={`/teams/${props.homeTeam.team_id}`}>
-                                <img className={classes.img} alt="complex" src={props.homeTeam.logo} />
-                            </Link>
-                        </ButtonBase>
-                        <Typography variant="body2" gutterBottom align='center'>
-                            {props.homeTeam.team_name}
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={12} sm container>
-                        <Grid item>
-                            <Typography variant="subtitle1">Score Full-time: <b>{props.score.fulltime}</b></Typography>
-                            <Typography variant="subtitle1">Score Half-time: <b>{props.score.halftime}</b></Typography>
-                        </Grid>
-                        <Grid item>
-                            <Typography variant="body2" color="textSecondary">
-                                {props.round}
-                            </Typography>
-                            <Typography variant="body2" color="textSecondary">
-                                {props.venue}
-                            </Typography>
-                        </Grid>
-                    </Grid>
-                    <Grid item>
-                        <ButtonBase className={classes.image}>
-                            <Link to={`/teams/${props.awayTeam.team_id}`}>
-                                <img className={classes.img} alt="complex" src={props.awayTeam.logo} />
-                            </Link>
-                        </ButtonBase>
-                        <Typography variant="body2" gutterBottom  align='center'>
-                            {props.awayTeam.team_name}
-                        </Typography>
-                    </Grid>
-                </Grid>
-            </Paper>
+
+            <ExpansionPanel className={classes.paper}>
+                <ExpansionPanelSummary
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                >
+                    <FixtureItem {...props} />
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
+                    <Typography>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                        sit amet blandit leo lobortis eget.
+            </Typography>
+                </ExpansionPanelDetails>
+            </ExpansionPanel>
+
         </div>
     );
 }
 
+const FixtureItem = props => {
+    const classes = useStyles();
 
-export default FixtureItem;
+    return (
+
+        <Grid container spacing={4}>
+            <Grid item>
+                <ButtonBase className={classes.image}>
+                    <Link to={`/teams/${props.homeTeam.team_id}`}>
+                        <img className={classes.img} alt="complex" src={props.homeTeam.logo} />
+                    </Link>
+                </ButtonBase>
+                <Typography variant="body2" gutterBottom align='center'>
+                    {props.homeTeam.team_name}
+                </Typography>
+            </Grid>
+            <Grid xs={12} sm container>
+                <Grid item xs={4}>
+                    <Typography variant="subtitle1" >Score Full-time: <b>{props.score.fulltime}</b></Typography>
+                    <Typography variant="subtitle1">Score Half-time: <b>{props.score.halftime}</b></Typography>
+                </Grid>
+                <Grid item>
+                    <Typography variant="body2" color="textSecondary">
+                        {props.round}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary">
+                        {props.venue}
+                    </Typography>
+                </Grid>
+            </Grid>
+            <Grid item>
+                <ButtonBase className={classes.image}>
+                    <Link to={`/teams/${props.awayTeam.team_id}`}>
+                        <img className={classes.img} alt="complex" src={props.awayTeam.logo} />
+                    </Link>
+                </ButtonBase>
+                <Typography variant="body2" gutterBottom align='center'>
+                    {props.awayTeam.team_name}
+                </Typography>
+            </Grid>
+        </Grid>
+
+    );
+}
+
+
+export default FixtureExpansionPanel;

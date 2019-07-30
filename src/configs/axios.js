e2e04fbc9e21353d080727495bfd3242abde5abc
KushnerykPavel <levelup.kpi@gmail.com>
2019-07-30 17:26:23 +0300

    Specific yarn version

--- src/configs/axios.js
+++ src/configs/axios.js
@@ -5,7 +5,7 @@ const instance = axios.create({
     baseURL: 'https://api-football-v1.p.rapidapi.com/v2'
 });
 
-instance.defaults.headers.common['X-RapidAPI-Key'] = config.token
+instance.defaults.headers.common['X-RapidAPI-Key'] = '3bc3316c2dmsh8e41b1e957eb5b0p1ba874jsn673415bd6a58'
 instance.defaults.headers.common['Accept'] = 'application/json'
 
 export default instance;
